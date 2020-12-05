93f1e3659e4e5629884f9a17ddc98a3101ec20df
NightlyFlux <nightlyflux@gmail.com>
2020-12-04 18:01:39 -0800

    Corregida la verificacion de contraseñas hasheadas

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -1,27 +1,36 @@
 "use strict";
 
+const Hash = use("Hash");
 const User = use("App/Models/User");
 
 class UserController {
   async login({ request, response, auth }) {
     try {
       const { email, password } = request.all();
+      console.log(request.all());
       const user = await User.query()
         .where({ email: email.toLowerCase() })
         .first();
-      let res = {};
-      // const passwordVerified = await Hash.verify(password, user.password);
-      // if (!passwordVerified) {
-      //   return response.status(502).json({ message: "Contraseña incorrecta" });
-      // }
-      const token = await auth.generate(user);
-      res.token = token.token;
-      res.user = {
-        id: user.id,
-        email: user.email,
-        user_type: user.user_type,
-      };
-      return response.status(200).json(res);
+      if (user) {
+        const passwordVerified = await Hash.verify(password, user.password);
+        if (passwordVerified) {
+          let res = {};
+          const token = await auth.generate(user);
+          res.token = token.token;
+          res.user = {
+            id: user.id,
+            email: user.email,
+            user_type: user.user_type,
+          };
+          return response.status(200).json(res);
+        } else {
+          return response
+            .status(502)
+            .json({ message: "Contraseña incorrecta" });
+        }
+      } else {
+        return response.status(404).json({ message: "El usuario no existe" });
+      }
     } catch (error) {
       return response.status(501).json({ message: "Ha ocurrido un error!" });
     }
